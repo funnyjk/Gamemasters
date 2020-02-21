6d3c1d1e489e22b35bd1802caa416215079ad0db
scott.shevrin <scott.shevrin@protegrity.com>
2020-02-21 11:48:17 -0500

    Site under construction

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -2,6 +2,8 @@ import React, {useContext, useEffect} from 'react';
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 import Context from "../../context/pageContext/context";
 import {setPage} from "../../context/pageContext/actions";
+import logo from "../../logo.png";
+import {Container} from "@material-ui/core";
 
 const HomePage = () => {
   const {dispatch} = useContext(Context);
@@ -11,9 +13,11 @@ const HomePage = () => {
   },[]);
 
   return (
-    <div>
-      Home Page!
-    </div>
+    <Container>
+      <img className={"logo"} src={logo}/>
+      <br/>
+      <strong>SITE UNDER CONSTRUCTION</strong>
+    </Container>
   );
 };
 
